cbb9974832b7f0464302ed337bfbe6d2c46ab641
Arnav K <arnavkaushal09@gmail.com>
2024-05-03 18:05:22 +0000

    started readme

--- src/pages/dashboard/index.ts
+++
@@ -1,7 +0,0 @@
-// imports
-import type { APIRoute } from "astro";
-
-// GET /dashboard -> /dashboard/overview
-export const GET = (async ({ redirect }) => {
-  return redirect("/dashboard/overview");
-}) as APIRoute;
